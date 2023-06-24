58362b969c5750e348292cc6431c6d011dbab24c
Amulya Parab <amulyaparab101@gmail.com>
2023-06-24 19:56:51 +0530

    styles: improved posts UI

--- src/Pages/Posts.js
+++ src/Pages/Posts.js
@@ -2,7 +2,7 @@ import { forumData } from "../Database/forumData";
 
 export const Posts = () => {
   return (
-    <div>
+    <div className="all-posts">
       <h1>Posts</h1>
       {forumData?.posts?.map(
         ({
@@ -19,23 +19,30 @@ export const Posts = () => {
           comments,
           isBookmarked,
         }) => (
-          <div key={postId}>
-            <div>
-              <img />
-              <p>Posted By</p>
-              <p>@{username}</p>
+          <div className="post" key={postId}>
+            <div className="votes">
+              <i class="fa-solid fa-caret-up"></i>
+              <p>{upvotes}</p>
+              <i class="fa-solid fa-caret-down"></i>
             </div>
-            <h2>{post}</h2>
             <div>
-              {tags.map((tag) => (
-                <p>{tag}</p>
-              ))}
-            </div>
-            <p>{postDescription}</p>
-            <div>
-              <i class="fa-regular fa-comment"></i>
-              <i class="fa-solid fa-share-nodes"></i>
-              <i class="fa-solid fa-bookmark"></i>
+              <div className="post-sections">
+                <img src={picUrl} alt={username} />
+                <p>Posted By</p>
+                <p>@{username}</p>
+              </div>
+              <h2>{post}</h2>
+              <div className="post-sections">
+                {tags.map((tag) => (
+                  <small className="tags">{tag}</small>
+                ))}
+              </div>
+              <p>{postDescription}</p>
+              <div className="post-sections actions">
+                <i class="fa-regular fa-comment"></i>
+                <i class="fa-solid fa-share-nodes"></i>
+                <i class="fa-solid fa-bookmark"></i>
+              </div>
             </div>
           </div>
         )
